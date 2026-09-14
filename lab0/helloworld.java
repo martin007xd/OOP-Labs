94b98198115b4de9c78b49c784dcf262f3428770
Kantavit <6930613006@psu.ac.th>
2026-09-14 16:02:52 +0700

    tarin

--- lab0/helloworld.java
+++ lab0/helloworld.java
@@ -3,5 +3,6 @@ public class helloworld{
     public static void main(String[] args) {
         System.out.println("HELLO TARIN");
         System.out.println("Hello tarin");
+        System.out.println("Tarin 1");
     }
 }
