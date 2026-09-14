3d9fad474e367ef6173d2038f5579e4c6ac951ad
Kantavit <6930613006@psu.ac.th>
2026-09-14 15:56:42 +0700

    Add Hello Tarin

--- lab0/helloworld.java
+++ lab0/helloworld.java
@@ -2,5 +2,6 @@ package lab0;
 public class helloworld{
     public static void main(String[] args) {
         System.out.println("HELLO TARIN");
+        System.out.println("Hello tarin");
     }
 }
